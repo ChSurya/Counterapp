b651078c5b1f385a0a904479a477d1daf2246802
Surya <chilukurisurya999@gmail.com>
2024-06-17 22:32:54 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Content from './Content';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter application</h1>
+      <Content/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Content.js
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+const Content = () =>{
+    const [count,setCount] = useState(0);
+    const increaseNum = ()=>{
+        setCount(count+1)
+        
+    }
+
+    const decreaseNum = () =>{
+        setCount(count-1);
+        console.log(count-1);
+    }
+
+    const resetNum = () =>{
+        setCount(0);
+    }
+
+    return (
+        // <div>
+        //     <h1>0</h1>
+        // </div>
+        
+        <div>
+            
+            <h2>{count}</h2>
+            <button onClick={increaseNum}>Increase</button>
+            <div>
+                <button onClick = {decreaseNum}>Decrease</button>
+            </div>
+            <div>
+                <button onClick={resetNum}>
+                    Reset
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
